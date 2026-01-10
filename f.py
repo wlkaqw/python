bb484b1a70145d7130f75bcd0005cf887e3ec1b2
wlkaqw <1937393403@qq.com>
2026-01-10 18:07:22 +0800

    1

--- f.py
+++ f.py
@@ -1,17 +1,20 @@
-n=int(input())
-for i in range(n):
-    s = list(map(int, input().split()))
-    count = 0
-    for j in s:
-        if s.count(j)==2:
-            pass
-        if s.count(j)==1:
-            count+=1
-    if count>1:
-        print('No')
+n = int(input())
+for _ in range(n):
+    s = input().strip()
+    char_count = {}
+    # 统计每个字符的出现次数
+    for c in s:
+        char_count[c] = char_count.get(c, 0) + 1
+    # 统计出现次数为奇数的字符数量
+    odd_count = 0
+    for cnt in char_count.values():
+        if cnt % 2 != 0:
+            odd_count += 1
+    # 判断是否符合回文条件
+    if (len(s) % 2 == 0 and odd_count == 0) or (len(s) % 2 == 1 and odd_count == 1):
+        print("Yes")
     else:
-        print('Yes')
-
+        print("No")
 
 
 
--- g.py
+++ g.py
@@ -13,4 +13,4 @@ for i in range(1,n-1):
         count+=0
     else:
         count+=3
-print(count,end='')
+print(count)
